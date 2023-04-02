d0f5a87c796c7e20cefae74e6d6b5e2c9fa226c2
joaopinto339 <62262268+joaopinto339@users.noreply.github.com>
2023-04-02 17:06:28 +0100

    DB done

--- api/src/main/sql/createTables.sql
+++ api/src/main/sql/createTables.sql
@@ -9,6 +9,66 @@ create table cinescope.users (
     state varchar(10) NOT NULL check (state in ('Active', 'Inactive'))
 );
 
-create table cinescope.movieslist (
-    
+create table cinescope.moviesData (
+    mimdbid varchar(100) primary key,
+    mtmdbid varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
+    image varchar(100) NOT NULL
 );
+
+create table cinescope.movieUserData (
+    mimdbid varchar(100) NOT NULL REFERENCES cinescope.moviesData,
+    userId int NOT NULL REFERENCES cinescope.users,
+    state varchar(20) NOT NULL check ( state in ('PTW', 'WATCHED'))
+);
+
+create table cinescope.moviesLists (
+    mlid int primary key GENERATED ALWAYS AS IDENTITY,
+    userid int references cinescope.users,
+    name varchar(100) NOT NULL
+);
+
+create table cinescope.movieList (
+    mimdbid varchar(100) NOT NULL REFERENCES cinescope.moviesData,
+    mlid int NOT NULL REFERENCES cinescope.moviesLists
+);
+
+create table cinescope.seriesData (
+    simdbid varchar(100) primary key,
+    stmdbid varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
+    image varchar(100) NOT NULL
+);
+
+create table cinescope.episodesData (
+    epimdbid varchar(100) primary key,
+    eptmdbid varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
+    image varchar(100) NOT NULL,
+    season int NOT NULL,
+    episode int not null
+);
+
+create table cinescope.seriesUserData (
+    eplid int primary key GENERATED ALWAYS AS IDENTITY,
+    simdbid varchar(100) NOT NULL REFERENCES cinescope.seriesData,
+    userid int NOT NULL REFERENCES cinescope.users,
+    state varchar(100) NOT NULL check (state in ('PTW', 'Watched', 'Watching'))
+);
+
+create table cinescope.watchedEpisodeList (
+    eplid int NOT NULL REFERENCES cinescope.seriesUserData,
+    epimdbid varchar(100) NOT NULL REFERENCES cinescope.episodesData
+);
+
+create table cinescope.seriesLists (
+    slid int primary key GENERATED ALWAYS AS IDENTITY,
+    userid int NOT NULL references cinescope.users,
+    name varchar(100) NOT NULL
+);
+
+create table cinescope.serieList (
+    slid int NOT NULL REFERENCES cinescope.seriesLists,
+    simdbid varchar(100) NOT NULL REFERENCES cinescope.seriesData
+);
+
