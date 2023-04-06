3d785f7de0a737574e3722771a3847e35c9a05b7
joaopinto339 <62262268+joaopinto339@users.noreply.github.com>
2023-04-06 18:10:45 +0100

    Wrong import on MoviesController

--- api/src/main/kotlin/pt/isel/ps/cinescope/controllers/MoviesController.kt
+++ api/src/main/kotlin/pt/isel/ps/cinescope/controllers/MoviesController.kt
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import pt.isel.ps.cinescope.controllers.models.MoviesModel
-import pt.isel.ps.cinescope.controllers.models.SeriesModel
 
 @RestController
 class MoviesController(val moviesServices: MoviesServices) {
