29e674834b3f3bf3341b4c6427bc70ea15bc8525
JoaoBalsinha <jeabalsinha@gmail.com>
2023-04-11 17:21:01 +0100

    Update MovieState

--- api/src/main/kotlin/pt/isel/ps/cinescope/domain/Movie.kt
+++ api/src/main/kotlin/pt/isel/ps/cinescope/domain/Movie.kt
@@ -1,5 +1,7 @@
 package pt.isel.ps.cinescope.domain
 
+import pt.isel.ps.cinescope.services.exceptions.BadRequestException
+
 data class Movie(val imdbId: String?, val tmdbId: String?, val name: String?, val img: String?)
 
 fun checkMovieState(state: String?):Boolean{
@@ -13,10 +15,11 @@ enum class MovieState(val state: String) {
     Watched("Watched");
 
     companion object {
-        fun fromString(string: String) =
+        fun fromString(string: String?) =
             when(string) {
                 "PTW" -> PTW
                 "Watched" -> Watched
+                null -> BadRequestException("Movie State cannot be null")
                 else -> throw IllegalArgumentException("Movie State does not exists")
             }
     }
