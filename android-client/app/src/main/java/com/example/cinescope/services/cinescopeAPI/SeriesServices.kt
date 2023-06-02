4e83a98e654504be3a78e704696f8af6f9f4fb11
jorgepires46023 <a46023@alunos.isel.pt>
2023-06-02 16:15:55 +0100

    Series Services Init

--- android-client/app/src/main/java/com/example/cinescope/services/cinescopeAPI/SeriesServices.kt
+++ android-client/app/src/main/java/com/example/cinescope/services/cinescopeAPI/SeriesServices.kt
@@ -1,7 +1,15 @@
 package com.example.cinescope.services.cinescopeAPI
 
-import com.example.cinescope.domain.searches.Series
+import com.example.cinescope.domain.content.ContentList
+import com.example.cinescope.domain.content.EmptyData
+import com.example.cinescope.domain.content.EpisodeData
+import com.example.cinescope.domain.content.ListId
+import com.example.cinescope.domain.content.SeriesData
+import com.example.cinescope.domain.content.UserDataContent
+import com.example.cinescope.services.MethodHTTP
 import com.example.cinescope.services.serviceInterfaces.CinescopeSeriesServices
+import com.example.cinescope.utils.joinPathWithVariables
+import com.example.cinescope.utils.send
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import java.net.URL
@@ -11,51 +19,82 @@ class SeriesServices(
     gson: Gson,
     httpClient: OkHttpClient
 ) : CinescopeSeriesServices, CinescopeServices(gson, httpClient) {
-    override suspend fun addSeriesToList(seriesId: Int, listId: Int): Series {
+    override suspend fun addSeriesToList(seriesId: Int, listId: Int, token: String): EmptyData {
+        val request = buildRequest(
+            url = cinescopeURL
+                .joinPathWithVariables(Series.ADD_SERIE, listOf(seriesId.toString(),listId.toString())),
+            method = MethodHTTP.POST,
+            token = token
+        )
+        //TODO handle this exceptions with our errors(try-catch)
+        return httpClient.send(request){ response ->
+            handleResponse(response, EmptyData::class.java)
+        }
+    }
+
+    override suspend fun changeSeriesState(seriesId: Int, state: String, token: String): EmptyData {
         TODO("Not yet implemented")
     }
 
-    override suspend fun changeSeriesState(seriesId: Int, listId: Int): Series {
+    override suspend fun deleteStateFromSeries(seriesId: Int, token: String): EmptyData {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getSeriesByState(seriesId: Int, listId: Int): Series {
+    override suspend fun deleteSeriesFromList(
+        seriesId: Int,
+        listId: Int,
+        token: String
+    ): EmptyData {
         TODO("Not yet implemented")
     }
 
-    override suspend fun addWatchedEpisode(seriesId: Int, listId: Int): Series {
+    override suspend fun addWatchedEpisode(
+        seriesId: Int,
+        seasonNr: Int,
+        epNumber: Int,
+        token: String
+    ): EmptyData {
         TODO("Not yet implemented")
     }
 
-    override suspend fun removeWatchedEpisode(seriesId: Int, listId: Int): Series {
+    override suspend fun deleteSeriesList(seriesId: Int, token: String): EmptyData {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getWatchedEpList(seriesId: Int, listId: Int): Series {
+    override suspend fun deleteWatchedEpisode(
+        seriesId: Int,
+        seasonNr: Int,
+        epNumber: Int,
+        token: String
+    ): EmptyData {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getSeriesLists(seriesId: Int, listId: Int): Series {
+    override suspend fun getAllSeriesByState(state: String, token: String): List<SeriesData> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getSeriesList(seriesId: Int, listId: Int): Series {
+    override suspend fun getAllSeriesLists(token: String): List<ContentList> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun createSeriesList(seriesId: Int, listId: Int): Series {
+    override suspend fun getSeriesList(listId: Int, token: String): List<SeriesData> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteSeriesList(seriesId: Int, listId: Int): Series {
+    override suspend fun createSeriesList(name: String, token: String): ListId {
         TODO("Not yet implemented")
     }
 
-    override suspend fun deleteSeriesFromList(seriesId: Int, listId: Int): Series {
+    override suspend fun getSeriesUserData(seriesId: Int, token: String): List<UserDataContent> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun removeSeriesState(seriesId: Int, listId: Int): Series {
+    override suspend fun getAllWatchedEpFromSeries(
+        seriesId: Int,
+        token: String
+    ): List<EpisodeData> {
         TODO("Not yet implemented")
     }
+
 }
